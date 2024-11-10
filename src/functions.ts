aa52fb78c60f02f347b1687f6bbbf185a331a26b
liquid <13621351+liqtags@users.noreply.github.com>
2024-11-10 13:09:10 +0000

    Init trackMarketCap

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,8 @@
-export const log = (message: string) => {
-    console.log(message);
-};
+import { log } from "./shared/log.ts";
 
+export { log };
+/**
+ * Track market cap of tokens
+ * @description This function is used to track the market cap of tokens.
+ */
+export { trackMarketCap } from "./shared/trackMarketCap/index.ts";
